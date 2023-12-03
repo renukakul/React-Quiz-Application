43ea8c7aa23f9bb35272987c51f9f4321210d62d
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2023-12-03 16:23:40 -0500

    Quiz Complete page added

--- React-Quiz/src/components/Quiz.jsx
+++ React-Quiz/src/components/Quiz.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import QUESTIONS from '../questions.js'
+import QUIZCOMPLETELOGO from '/Users/renukakulkarni/Work/ReactAllProjects/React-Quiz-Application/React-Quiz/src/assets/quiz-complete.png'
 
 export default function Quiz(){
     const [userAns, setUserAns] = useState([])
     
     const currentQuestion = userAns.length;
+    const quizComplete = currentQuestion === QUESTIONS.length;
 
     function handleAnsButton(selectedAnswer){
         setUserAns((prevUserAns) => {
@@ -12,8 +14,14 @@ export default function Quiz(){
         });
     }
 
-
-
+    if (quizComplete){
+        return(
+            <div id='summary'>
+                <img src = {QUIZCOMPLETELOGO} alt="Trophy" />
+                <h2>Quiz Completed !</h2>
+            </div>
+        );
+    }
 
     return(
         <div id="quiz">
