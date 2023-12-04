c7f7a4aa54ba0bff9c42168a2943927503467ebd
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2023-12-03 22:22:47 -0500

    Added Timer component for setting 1 min timer to every Question

--- React-Quiz/src/components/Quiz.jsx
+++ React-Quiz/src/components/Quiz.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import QUESTIONS from '../questions.js'
 import QUIZCOMPLETELOGO from '/Users/renukakulkarni/Work/ReactAllProjects/React-Quiz-Application/React-Quiz/src/assets/quiz-complete.png'
+import Timer from './Timer.jsx';
+
+
+const TIMER = 60000;
 
 export default function Quiz(){
-    const [userAns, setUserAns] = useState([])
+    const [userAns, setUserAns] = useState([]);
     
     const currentQuestion = userAns.length;
     const quizComplete = currentQuestion === QUESTIONS.length;
-
-    function handleAnsButton(selectedAnswer){
+    
+    const handleAnsSelect =  useCallback(function handleAnsButton(selectedAnswer){
         setUserAns((prevUserAns) => {
             return [...prevUserAns, selectedAnswer];
         });
-    }
+    },[]);
+
+    const handleSkipAnswer = useCallback(() => handleAnsSelect(null), [handleAnsSelect]);
+
 
     if (quizComplete){
         return(
@@ -26,17 +33,17 @@ export default function Quiz(){
     return(
         <div id="quiz">
             <div id="question">
+                <Timer key={currentQuestion} timeout={TIMER} onTimeout={handleSkipAnswer}  />
                 <h2>{QUESTIONS[currentQuestion].text}</h2>
                 <ol id="answers">
                     {QUESTIONS[currentQuestion].answers.map((ans) => (
                         <li key={ans} className='answer'>
-                            <button onClick={() => handleAnsButton(ans)}>{ans}</button>
+                            <button onClick={() => handleAnsSelect(ans)}>{ans}</button>
                         </li>
                     ))}
                 </ol>            
-            
             </div>
-
+            <Timer timer={TIMER} />              
         </div>
     );
 }
